1a0b90672fe39c34837c0ab4149071fe7fe2cd00
unknown <you@example.com>
2021-04-14 20:01:04 +0530

    first post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `"blog" is not defined`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Karan Yadav`,
+      summary: `who lives and works in Mumbai building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      github: `alucard17`,
     },
   },
   plugins: [
